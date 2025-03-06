37d4b85c3f040eae73b276010d69b08a0b38a439
Foyezes <106163842+Foyezes@users.noreply.github.com>
2025-03-06 17:35:08 +0600

    Update vectorTransform.gd

--- vectorTransform.gd
+++ vectorTransform.gd
@@ -27,12 +27,14 @@ func _get_output_port_type(port):
 func _get_return_icon_type():
 	return VisualShaderNode.PORT_TYPE_VECTOR_3D
 func _get_property_count():
-	return 2
+	return 3
 func _get_property_default_index(index):
 	match index:
 		0:
 			return 0
 		1:
+			return 0
+		2:
 			return 0
 func _get_property_name(index):
 	match index:
@@ -40,6 +42,8 @@ func _get_property_name(index):
 			return "Input"
 		1:
 			return "Output"
+		2:
+			return "Method"
 func _get_property_options(index):
 	match index:
 		0:
@@ -55,24 +59,82 @@ func _get_property_options(index):
 					return ["Model Space", "World Space", "Clip Space"]
 				3:
 					return ["View Space"]
+		2:
+			return ["AxB", "BxA", "AxB(3x3)", "BxA(3x3)"]
 
 func _get_code(input_vars, output_vars, mode, type):
 	var input = get_option_index(0)
 	var output = get_option_index(1)
-	match [input, output]:
-		[0, 0]:#Model Space to World Space
+	var method = get_option_index(2)
+	match [input, output, method]:
+		#AxB
+		[0, 0, 0]:#Model Space to World Space
 			return output_vars[0] + "=vec3((MODEL_MATRIX * vec4(%s, 1.0)).xyz);" % input_vars[0]
-		[1, 0]:#World Space to Model Space
+		[1, 0, 0]:#World Space to Model Space
 			return output_vars[0] + "=vec3(((inverse(MODEL_MATRIX)) * vec4(%s, 1.0)).xyz);" % input_vars[0]
-		[0, 1]:#Model Space to View Space
+		[0, 1, 0]:#Model Space to View Space
 			return output_vars[0] + "=vec3((MODELVIEW_MATRIX * vec4(%s, 1.0)).xyz);" % input_vars[0]
-		[2, 0]:#View Space to Model Space
+		[2, 0, 0]:#View Space to Model Space
 			return output_vars[0] + "=vec3(((inverse(MODELVIEW_MATRIX)) * vec4(%s, 1.0)).xyz);" % input_vars[0]
-		[1, 1]:#World Space to View Space
+		[1, 1, 0]:#World Space to View Space
 			return output_vars[0] + "=vec3((VIEW_MATRIX * vec4(%s, 1.0)).xyz);" % input_vars[0]
-		[2, 1]:#View Space to World Space
+		[2, 1, 0]:#View Space to World Space
 			return output_vars[0] + "=vec3((INV_VIEW_MATRIX * vec4(%s, 1.0)).xyz);" % input_vars[0]
-		[2, 2]:#View Space to Clip Space
+		[2, 2, 0]:#View Space to Clip Space
 			return output_vars[0] + "=vec3((PROJECTION_MATRIX * vec4(%s, 1.0)).xyz);" % input_vars[0]
-		[3, 0]:#Clip Space to View Space
+		[3, 0, 0]:#Clip Space to View Space
 			return output_vars[0] + "=vec3((INV_PROJECTION_MATRIX * vec4(%s, 1.0)).xyz);" % input_vars[0]
+			
+		#BxA
+		[0, 0, 1]:  # Model Space to World Space
+			return output_vars[0] + "=vec3((vec4(%s, 1.0) * MODEL_MATRIX).xyz);" % input_vars[0]
+		[1, 0, 1]:  # World Space to Model Space
+			return output_vars[0] + "=vec3((vec4(%s, 1.0) * inverse(MODEL_MATRIX)).xyz);" % input_vars[0]
+		[0, 1, 1]:  # Model Space to View Space
+			return output_vars[0] + "=vec3((vec4(%s, 1.0) * MODELVIEW_MATRIX).xyz);" % input_vars[0]
+		[2, 0, 1]:  # View Space to Model Space
+			return output_vars[0] + "=vec3((vec4(%s, 1.0) * inverse(MODELVIEW_MATRIX)).xyz);" % input_vars[0]
+		[1, 1, 1]:  # World Space to View Space
+			return output_vars[0] + "=vec3((vec4(%s, 1.0) * VIEW_MATRIX).xyz);" % input_vars[0]
+		[2, 1, 1]:  # View Space to World Space
+			return output_vars[0] + "=vec3((vec4(%s, 1.0) * INV_VIEW_MATRIX).xyz);" % input_vars[0]
+		[2, 2, 1]:  # View Space to Clip Space
+			return output_vars[0] + "=vec3((vec4(%s, 1.0) * PROJECTION_MATRIX).xyz);" % input_vars[0]
+		[3, 0, 0]:  # Clip Space to View Space
+			return output_vars[0] + "=vec3((vec4(%s, 1.0) * INV_PROJECTION_MATRIX).xyz);" % input_vars[0]
+		
+		#AxB(3x3)
+		[0, 0, 2]:#Model Space to World Space
+			return output_vars[0] + "=vec3((MODEL_MATRIX * vec4(%s, 0.0)).xyz);" % input_vars[0]
+		[1, 0, 2]:#World Space to Model Space
+			return output_vars[0] + "=vec3(((inverse(MODEL_MATRIX)) * vec4(%s, 0.0)).xyz);" % input_vars[0]
+		[0, 1, 2]:#Model Space to View Space
+			return output_vars[0] + "=vec3((MODELVIEW_MATRIX * vec4(%s, 0.0)).xyz);" % input_vars[0]
+		[2, 0, 2]:#View Space to Model Space
+			return output_vars[0] + "=vec3(((inverse(MODELVIEW_MATRIX)) * vec4(%s, 0.0)).xyz);" % input_vars[0]
+		[1, 1, 2]:#World Space to View Space
+			return output_vars[0] + "=vec3((VIEW_MATRIX * vec4(%s, 0.0)).xyz);" % input_vars[0]
+		[2, 1, 2]:#View Space to World Space
+			return output_vars[0] + "=vec3((INV_VIEW_MATRIX * vec4(%s, 0.0)).xyz);" % input_vars[0]
+		[2, 2, 2]:#View Space to Clip Space
+			return output_vars[0] + "=vec3((PROJECTION_MATRIX * vec4(%s, 0.0)).xyz);" % input_vars[0]
+		[3, 0, 2]:#Clip Space to View Space
+			return output_vars[0] + "=vec3((INV_PROJECTION_MATRIX * vec4(%s, 0.0)).xyz);" % input_vars[0]
+			
+		#BxA(3x3)
+		[0, 0, 3]:  # Model Space to World Space
+			return output_vars[0] + "=vec3((vec4(%s, 0.0) * MODEL_MATRIX).xyz);" % input_vars[0]
+		[1, 0, 3]:  # World Space to Model Space
+			return output_vars[0] + "=vec3((vec4(%s, 0.0) * inverse(MODEL_MATRIX)).xyz);" % input_vars[0]
+		[0, 1, 3]:  # Model Space to View Space
+			return output_vars[0] + "=vec3((vec4(%s, 0.0) * MODELVIEW_MATRIX).xyz);" % input_vars[0]
+		[2, 0, 3]:  # View Space to Model Space
+			return output_vars[0] + "=vec3((vec4(%s, 0.0) * inverse(MODELVIEW_MATRIX)).xyz);" % input_vars[0]
+		[1, 1, 3]:  # World Space to View Space
+			return output_vars[0] + "=vec3((vec4(%s, 0.0) * VIEW_MATRIX).xyz);" % input_vars[0]
+		[2, 1, 3]:  # View Space to World Space
+			return output_vars[0] + "=vec3((vec4(%s, 0.0) * INV_VIEW_MATRIX).xyz);" % input_vars[0]
+		[2, 2, 3]:  # View Space to Clip Space
+			return output_vars[0] + "=vec3((vec4(%s, 0.0) * PROJECTION_MATRIX).xyz);" % input_vars[0]
+		[3, 0, 3]:  # Clip Space to View Space
+			return output_vars[0] + "=vec3((vec4(%s, 0.0) * INV_PROJECTION_MATRIX).xyz);" % input_vars[0]
